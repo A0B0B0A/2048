c86a269ff17761fed296e2a7524937f50430f4db
A0B0B0A <152081588+A0B0B0A@users.noreply.github.com>
2024-05-03 20:02:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,227 +1,303 @@
 import pygame
 import random
 
-
 pygame.init()
-
-# pygame.mixer.init()
-# pygame.mixer.music.load('')
-# pygame.mixer.music.play()
-# pygame.mixer.music.set_volume(0.3)
+pygame.mixer.init()
+pygame.mixer.music.load('perfect-beauty.mp3')
+pygame.mixer.music.play()
+pygame.mixer.music.set_volume(0.3)
 
 WIDTH, HEIGHT = 500, 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 90
 clock = pygame.time.Clock()
 pygame.display.set_caption("2048")
-font = pygame.font.Font('freesansbold.ttf', 30) 
+font = pygame.font.Font('freesansbold.ttf', 30)
 
-colors = {0: ("#D6D6DA"),
-          2:   ("#E5DFD9"),
-          4:   ("#ECCEB1"),
-          8:   ("#EFBF8F"),
-          16:  ("#EEBC8B"),
-          32:  ("#F28B0C"),
-          64:  ("#EE4C40"),
-          128: ("#EDEB8B"),
-          256: ("#EDEB74"),
-          512: ("#EDEB5A"),
-          1024:("#F3D65F"),
-          2048:("#FEFA0A"),
-          'dark_text': ("#8B7066"),
-          'light_text': ("#FFFFFF"),
-          'background': ("#B6B6C0"),
-          'black': ("#000000")
+colors = {
+    0: "#D6D6DA",
+    2: "#E5DFD9",
+    4: "#ECCEB1",
+    8: "#EFBF8F",
+    16: "#EEBC8B",
+    32: "#F28B0C",
+    64: "#EE4C40",
+    128: "#EDEB8B",
+    256: "#EDEB74",
+    512: "#EDEB5A",
+    1024: "#F3D65F",
+    2048: "#FEFA0A",
+    'dark_text': "#8B7066",
+    'light_text': "#FFFFFF",
+    'background': "#B6B6C0",
+    'black': "#000000"
 }
 
-board_value = [[0 for _ in range (4)]for _ in range(4)]
+board_value = [[0 for _ in range(4)] for _ in range(4)]
 game_over = False
-spawn_new = True
-init_count = 0
 direction = ""
-score = 0 
+score = 0
 file = open("hight_score", "r")
-init_hight_score = int(file.readline())
+init_high_score = int(file.readline())
 file.close()
-hight_score = init_hight_score
+high_score = init_high_score
 
+black = '#000000'
+light_yellow = '#FFECA1'
+dark_yellow = '#FFDE59'
 
-def draw_over():
-    pygame.draw.rect(window, "#000000", [50, 50, 300, 100], 0 , 10)
-    game_over_text1 = font.render("Game Over!", True, "#ffffff")
-    game_over_text2 = font.render("Press ENTER to restart!", True, "#ffffff")
-    window.blit(game_over_text1, (130, 65))
-    window.blit(game_over_text2, (70, 105))
+class Button:
+    def __init__(self, text, position, font, color, text_color, action):
+        self.text = text
+        self.position = position
+        self.font = font
+        self.color = color
+        self.text_color = text_color
+        self.action = action
+        self.label = self.font.render(self.text, True, self.text_color)
+        self.rect = pygame.Rect(self.position[0], self.position[1], self.label.get_width() + 20, self.label.get_height() + 20)
 
-def take_turn(direc, board):
-    global score
-    merged = [[False for _ in range (4)] for _ in range(4)]
-    
-    if direc == "UP":
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.label, (self.position[0] + 10, self.position[1] + 10))
+
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos)
+
+def action1():
+    global game_start
+    game_start = True
+    init_game()
+
+def action2():
+    print("Вибрано кнопку 2")
+
+def action3():
+    global run
+    run = False
+
+def music_on_off():
+    global music_playing
+    global button4
+    if music_playing:
+        pygame.mixer.music.stop()
+        music_playing = False
+        button4.text = "Music Off"
+    else:
+        pygame.mixer.music.load("perfect-beauty.mp3")
+        pygame.mixer.music.play(-1)
+        music_playing = True
+        button4.text = "Music On"
+    button4.label = button4.font.render(button4.text, True, button4.text_color)
+
+button1 = Button("Start game", (173, 170), font, dark_yellow, black, action1)
+button2 = Button("Settings", (180, 270), font, dark_yellow, black, action2)
+button3 = Button("Exit", (210, 370), font, dark_yellow, black, action3)
+button4 = Button("Music On", (350, 550), font, dark_yellow, black, music_on_off)
+
+buttons = [button1, button2, button3, button4]
+
+def draw_board():
+    if game_start:
+        pygame.draw.rect(window, colors["background"], [0,0,500,500], 0, 10)
+        score_text = font.render(f'Score: {score}', True, 'black')
+        high_score_text = font.render(f'High Score: {high_score}', True, 'black')
+        window.blit(score_text, (10, 510))
+        window.blit(high_score_text, (10, 550))
+
+def draw_box(board):
+    if game_start:
         for i in range(4):
-            for j in range (4):
-                shift = 0
-                if i > 0:
-                    for q in range(i):# перевіряємо скільки рядків треба перевіряти
-                        if board[q][j] == 0:
-                            shift += 1
-                    if shift > 0:
-                        board[i - shift][j] = board[i][j]
-                        board[i][j] = 0 
-                    if board[i -shift - 1][j] == board[i - shift][j] and not merged[i - shift][j] and not merged[i - shift - 1][j]:
-                        board[i - shift - 1][j] *= 2
-                        score += board[i - shift - 1][j]
-                        board[i - shift][j] = 0
-                        merged[i - shift - 1][j] = True
-
- 
-    elif direc == "DOWN":
-        for i in range(3):
             for j in range(4):
-                shift  = 0
-                for q in range(i + 1):
-                    if board[3 - q][j] == 0:
-                        shift += 1
-                if shift > 0:
-                    board[2 - i + shift][j] = board[2 - i][j]
-                    board[2 - i][j] = 0
-                if 3 - i + shift <= 3:
-                    if board[2 - i + shift][j] == board[3 - i + shift][j] and not merged[3 - i + shift][j] and not merged[2 - i + shift][j]:
-                        board[3 - i + shift][j] *= 2
-                        score += board[3 - i + shift][j]                     
-                        board[2 - i + shift][j] = 0
-                        merged[3 - i + shift][j] = True
-
-
-    elif direc == "LEFT":
-        for i in range(4):
-            for j in range(4):
-                shift = 0
-                for q in range(j):
-                    if board[i][q] == 0:
-                        shift += 1
-                if shift > 0:
-                    board[i][j - shift] = board[i][j]
-                    board[i][j] = 0
-                if board[i][j - shift] == board[i][j - shift - 1] and not merged[i][j - shift - 1] and not merged[i][j - shift]:
-                    board[i][j - shift - 1] *= 2
-                    score += board[i][j - shift - 1]
-                    board[i][j - shift] = 0
-                    merged[i][j - shift - 1] = True 
-
-
-    elif direc == "RIGHT":
-        for i in range(4):
-            for j in range(4):
-                shift = 0 
-                for q in range(j):
-                    if board[i][3 - q] == 0:
-                        shift += 1
-                if shift > 0:
-                    board[i][3 - j + shift] == board[i][3 - j]
-                    board[i][3 - j] = 0
-                if 4 - j + shift <= 3:
-                    if board[i][4 - j + shift] == board[i][3 - j + shift] and not merged[i][4 - j + shift] and not merged[i][3 - j + shift]:
-                        board[i][4 - j + shift] *= 2
-                        score += board[i][4 - j + shift] 
-                        board[i][3 - j + shift] = 0
-                        merged[i][4 - j + shift] = True
-
-    return board  
-
+                value = board[i][j]
+                if value > 8:
+                    value_color = colors["light_text"]
+                else:
+                    value_color = colors["dark_text"]
+                if value <= 2048:
+                    color = colors[value]
+                else:
+                    color = colors["black"]
+                pygame.draw.rect(window, color, [j * 120 + 20, i * 120 + 20, 100, 100])
+                if value > 0:
+                    value_len = len(str(value))
+                    font = pygame.font.Font('freesansbold.ttf', 60 - (5 * value_len))
+                    value_text = font.render(str(value), True, value_color)
+                    text_rect = value_text.get_rect(center=(j *120 + 70, i * 120 + 70))
+                    window.blit(value_text, text_rect)
+                    pygame.draw.rect(window, 'black', [j * 120 + 20, i * 120 + 20, 100, 100], 4, 2)
 
 def new_box(board):
     count = 0
-    full = False
-    while any(0 in row for row in board) and count  < 1:
-        row = random.randint(0,3)
-        col = random.randint(0,3)
-        if board [row][col] == 0:
+    while any(0 in row for row in board) and count < 1:
+        row = random.randint(0, 3)
+        col = random.randint(0, 3)
+        if board[row][col] == 0:
             count += 1
-            if random.randint(1,10) == 10:
-                board [row][col] = 4 
+            if random.randint(1, 10) == 10:
+                board[row][col] = 4
             else:
-                board [row][col] = 2
-    if count < 1:
-        full = True
-    return board, full
+                board[row][col] = 2
+    if count == 1:  # Перевірка, чи з'явився лише один новий квадратик
+        return board
 
-def draw_board():
-    pygame.draw.rect(window, colors["background"], [0,0,500,500], 0, 10)# колір, розмір, товщина, заукруглення
-    score_text = font.render(f'Score: {score}', True, 'black')
-    hight_score_text = font.render(f'Hight score: {hight_score}', True, 'black')
-    window.blit(score_text, (10, 510))
-    window.blit(hight_score_text, (10, 550))
-
-
-def draw_box(board):
+def game_over_check(board):
+    # Перевірка, чи є вільні клітини
+    if any(0 in row for row in board):
+        return False
+    # Перевірка, чи можливі ще які-небудь ходи
     for i in range(4):
-        for j in range (4):
-            value = board [i][j] #цифра
-            if value > 8:
-                value_color = colors["light_text"]
-            else:
-                value_color = colors["dark_text"]
-            if value <= 2048:
-                color = colors[value]
-            else:
-                color = colors["black"]
-            pygame.draw.rect(window, color, [j * 120 + 20, i * 120  + 20, 100, 100])
-            if value > 0:
-                value_len = len(str(value))# довжина цифри
-                font = pygame.font.Font('freesansbold.ttf', 60 - ( 5 * value_len))
-                value_text = font.render(str(value), True, value_color)
-                text_rect = value_text.get_rect(center = (j *120 + 70, i * 120 + 70))
-                window.blit(value_text, text_rect)
-                pygame.draw.rect(window, 'black', [j * 120 + 20, i * 120  + 20, 100, 100],4,2)
+        for j in range(4):
+            if (i < 3 and board[i][j] == board[i + 1][j]) or \
+               (j < 3 and board[i][j] == board[i][j + 1]):
+                return False
+    return True
 
+def take_turn(direc, board):
+    global score
+    global game_over
+    merged = [[False for _ in range(4)] for _ in range(4)]
 
+    if direc == "UP":
+        for j in range(4):
+            for i in range(1, 4):
+                if board[i][j] != 0:
+                    row = i
+                    while row > 0 and board[row - 1][j] == 0:
+                        row -= 1
+                    if row != i:
+                        board[row][j] = board[i][j]
+                        board[i][j] = 0
+                    if row > 0 and board[row - 1][j] == board[row][j] and not merged[row - 1][j]:
+                        board[row - 1][j] *= 2
+                        score += board[row - 1][j]
+                        board[row][j] = 0
+                        merged[row - 1][j] = True
+ 
+    elif direc == "DOWN":
+        for j in range(4):
+            for i in range(2, -1, -1):
+                if board[i][j] != 0:
+                    row = i
+                    while row < 3 and board[row + 1][j] == 0:
+                        row += 1
+                    if row != i:
+                        board[row][j] = board[i][j]
+                        board[i][j] = 0
+                    if row < 3 and board[row + 1][j] == board[row][j] and not merged[row + 1][j]:
+                        board[row + 1][j] *= 2
+                        score += board[row + 1][j]                     
+                        board[row][j] = 0
+                        merged[row + 1][j] = True
+
+    elif direc == "LEFT":
+        for i in range(4):
+            for j in range(1, 4):
+                if board[i][j] != 0:
+                    col = j
+                    while col > 0 and board[i][col - 1] == 0:
+                        col -= 1
+                    if col != j:
+                        board[i][col] = board[i][j]
+                        board[i][j] = 0
+                    if col > 0 and board[i][col - 1] == board[i][col] and not merged[i][col - 1]:
+                        board[i][col - 1] *= 2
+                        score += board[i][col - 1]
+                        board[i][col] = 0
+                        merged[i][col - 1] = True 
+
+    elif direc == "RIGHT":
+        for i in range(4):
+            for j in range(2, -1, -1):
+                if board[i][j] != 0:
+                    col = j
+                    while col < 3 and board[i][col + 1] == 0:
+                        col += 1
+                    if col != j:
+                        board[i][col] = board[i][j]
+                        board[i][j] = 0
+                    if col < 3 and board[i][col + 1] == board[i][col] and not merged[i][col + 1]:
+                        board[i][col + 1] *= 2
+                        score += board[i][col + 1] 
+                        board[i][col] = 0
+                        merged[i][col + 1] = True
+    if game_over_check(board):
+        game_over = True
+    return board
+
+def init_game():
+    global board_value, score, game_over, high_score
+    board_value = [[0 for _ in range(4)] for _ in range(4)]
+    game_over = False
+    score = 0
+    if score > high_score:
+        high_score = score
+        file = open("hight_score", "w")
+        file.write(f'{high_score}')
+        file.close()
+    new_box(board_value)
+
+music_playing = True
+game_start = False
 run = True
+init_game()
 while run:
     clock.tick(FPS)
-    window.fill("#CECECE")
+    window.fill(light_yellow)
 
+    mouse_pos = pygame.mouse.get_pos()
     draw_board()
     draw_box(board_value)
-
-    if spawn_new or init_count < 2:
-        board_value, game_over = new_box(board_value)
-        spawn_new = False
-        init_count += 1
-    if direction != "":
-        board_value = take_turn(direction, board_value)
-        direction = ""
-        spawn_new = True
-        if game_over:
-            draw_over()
-            if hight_score > init_hight_score:
-                file = open("hight_score", "w")
-                file.write(f'{hight_score}')
-                file.close()
-                init_hight_score = hight_score
-
+    
+    if not game_start:
+        for button in buttons:
+            button.draw(window)
+    else:
+        if direction:
+            board_value = take_turn(direction, board_value)
+            new_box(board_value)
+            direction = ""
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                direction = "UP"
-            elif event.key == pygame.K_DOWN:
-                direction = "DOWN"
-            elif event.key == pygame.K_RIGHT:
-                direction = "RIGHT"
-            elif event.key == pygame.K_LEFT:
-                direction = "LEFT"
-            if game_over:
-                if event.key == pygame.K_RETURN:
-                    board_value = [[0 for _ in range(4)]for _ in range(4)]
-                    spawn_new = True
-                    init_count = 0
-                    score = 0
-                    direction = ""
-                    game_over = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if not game_over:
+                for button in buttons:
+                    if button.is_clicked(mouse_pos):
+                        button.action()
+        elif event.type == pygame.KEYDOWN:
+            if not game_over:
+                if event.key == pygame.K_UP:
+                    direction = "UP"
+                elif event.key == pygame.K_DOWN:
+                    direction = "DOWN"
+                elif event.key == pygame.K_RIGHT:
+                    direction = "RIGHT"
+                elif event.key == pygame.K_LEFT:
+                    direction = "LEFT"
+            for button in buttons:
+                    if button.text == "Restart" and event.key == pygame.K_r:
+                        button.action()
+                    elif button.text == "Exit" and event.key == pygame.K_q:
+                        button.action()
+            if game_over and event.key == pygame.K_RETURN:
+                init_game()
+    
+    if game_over:
+        window.fill('#E8E8E8')
+        # Вивід повідомлення про програш
+        font_game_over = pygame.font.Font('freesansbold.ttf', 40)
+        game_over_text = font_game_over.render("Game Over!", True, 'black')
+        window.blit(game_over_text, (150, 250))
+        # Додавання кнопок Restart і Exit
+        restart_button = Button("Restart", (150, 320), font, dark_yellow, black, init_game)
+        exit_button = Button("Exit", (300, 320), font, dark_yellow, black, action3)
+        restart_button.draw(window)
+        exit_button.draw(window)
 
-    if score > hight_score:
-        hight_score = score
-
+    # Оновлення тексту кнопки для музики
+    button4.label = button4.font.render(button4.text, True, button4.text_color)
+    button4.draw(window)
+    
     pygame.display.flip()
