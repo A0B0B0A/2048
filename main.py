7e325f80d6e520b28dd96946170dba1527cd8b8a
A0B0B0A <152081588+A0B0B0A@users.noreply.github.com>
2024-05-10 19:13:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 import random
 
-#створення музики
+# створення музики
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load('perfect-beauty.mp3')
 pygame.mixer.music.play()
 pygame.mixer.music.set_volume(0.3)
 
-
+# Зміна розміру вікна та завантаження зображень
 WIDTH, HEIGHT = 500, 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 90
@@ -16,13 +16,23 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("2048")
 font = pygame.font.Font("arial-unicode-ms.ttf", 30)
 
+# Завантаження зображень та визначення кольорів
 setting_img = pygame.image.load('settings_img.jpg')
 setting_img = pygame.transform.scale(setting_img, (WIDTH, HEIGHT))
 
 menu_img = pygame.image.load('menu.jpg')
 menu_img = pygame.transform.scale(menu_img, (WIDTH, HEIGHT))
 
-colors = {#словник кольорів
+lose_img = pygame.image.load('lose_img.jpg')
+lose_img = pygame.transform.scale(lose_img, (WIDTH, HEIGHT))
+
+win_img = pygame.image.load('win_img.jpg')
+win_img = pygame.transform.scale(win_img, (WIDTH, HEIGHT))
+
+loading_img = pygame.image.load('loading_img.jpg')
+loading_img = pygame.transform.scale(loading_img, (WIDTH, HEIGHT))
+
+colors = {  # словник кольорів
     0: "#D6D6DA",
     2: "#E5DFD9",
     4: "#ECCEB1",
@@ -40,11 +50,12 @@ colors = {#словник кольорів
     'background': "#B6B6C0",
     'black': "#000000",
     'light_yellow': '#FFECA1',
-    'dark_yellow' :  '#FFDE59',
+    'dark_yellow': '#FFDE59',
     'dark_gray': '#878585',
     'light_red': '#F7807A'
 }
 
+# Ініціалізація змінних гри
 board_value = [[0 for _ in range(4)] for _ in range(4)]
 game_over = False
 direction = ""
@@ -54,8 +65,7 @@ init_height_score = int(file.readline())
 file.close()
 height_score = init_height_score
 
-
-
+# Клас кнопки
 class Button:
     def __init__(self, text, position, font, color, text_color, action, sound=None):
         self.text = text
@@ -65,7 +75,8 @@ class Button:
         self.text_color = text_color
         self.action = action
         self.label = self.font.render(self.text, True, self.text_color)
-        self.rect = pygame.Rect(self.position[0], self.position[1], self.label.get_width() + 20, self.label.get_height() + 20)
+        self.rect = pygame.Rect(self.position[0], self.position[1], self.label.get_width() + 20,
+                                self.label.get_height() + 20)
         self.visible = True
 
     def draw(self, screen):
@@ -83,6 +94,8 @@ class Button:
     def show(self, screen):
         self.visible = True
 
+
+# Функція ініціалізації гри
 def init_game():
     global board_value
     global score
@@ -93,16 +106,19 @@ def init_game():
     new_box(board_value)
     new_box(board_value)
 
-#функції кнопок
+
+# Функції кнопок
 def start_game():
     global screen, game_start
     screen = 'game'
     game_start = True
     init_game()
 
+
 def go_to_settings():
     global screen
     screen = 'settings'
+
 
 def restart_game():
     global screen, game_over
@@ -110,9 +126,11 @@ def restart_game():
     game_over = False
     init_game()
 
+
 def exit_game():
     global run
     run = False
+
 
 def music_on_off():
     global music_playing
@@ -126,27 +144,33 @@ def music_on_off():
         music_btn.text = "Music On"
     music_btn.label = music_btn.font.render(music_btn.text, True, music_btn.text_color)
 
+
 def return_to_menu():
     global screen
     screen = 'menu'
 
-#кнопки
+
+# кнопки
 game_start_btn = Button("Start game", (173, 170), font, colors['light_red'], colors["black"], start_game)
 settings_btn = Button("Settings", (180, 270), font, colors['light_red'], colors["black"], go_to_settings)
-exit_btn = Button("Exit", (210, 370), font, colors['light_red'], colors["black"], exit_game())
+exit_btn = Button("Exit", (210, 370), font, colors['light_red'], colors["black"], exit_game)  # <-- ЗМІНА -->
 music_btn = Button("Music On", (355, 540), font, colors['light_red'], colors["black"], music_on_off)
-restart_button = Button("Restart", (150, 320), font, colors['light_red'], colors["black"], restart_game)
-exit_button = Button("Exit", (300, 320), font, colors['light_red'], colors["black"], exit_game)
-menu_button = Button(" \u21A9 ", (0,0), font, colors['dark_gray'], colors['black'], return_to_menu)
+restart_button = Button("Restart", (150, 450), font, colors['light_red'], colors["black"], restart_game)
+exit_button = Button("Exit", (300, 450), font, colors['light_red'], colors["black"], exit_game)
+menu_button = Button(" \u21A9 ", (0, 0), font, colors['dark_gray'], colors['black'], return_to_menu)
+
+# Лист кнопок
+buttons_menu = [game_start_btn, settings_btn, exit_btn, music_btn]
+buttons_gameover = [restart_button, exit_button, menu_button]
+buttons_win = [menu_button]
+buttons_settings = [menu_button]
 
 
-#лист кнопок
-buttons = [game_start_btn, settings_btn, exit_btn, music_btn]
 def draw_board():
     '''фукнція створення рахунків'''
     if game_start:
         window.fill(colors["background"])  # Зміна кольору фону
-        #створення та показ рахунків
+        # створення та показ рахунків
         score_text = font.render(f'Score: {score}', True, 'black')
         height_score_text = font.render(f'High Score: {height_score}', True, 'black')
         window.blit(score_text, (10, 510))
@@ -171,9 +195,10 @@ def draw_box(board):
                     value_len = len(str(value))
                     font = pygame.font.Font('calibri-font-family\calibri-bold.ttf', 60 - (5 * value_len))
                     value_text = font.render(str(value), True, value_color)
-                    text_rect = value_text.get_rect(center=(j *120 + 70, i * 120 + 70))
+                    text_rect = value_text.get_rect(center=(j * 120 + 70, i * 120 + 70))
                     window.blit(value_text, text_rect)
                     pygame.draw.rect(window, 'black', [j * 120 + 20, i * 120 + 20, 100, 100], 4, 2)
+
 
 def new_box(board):
     '''функція створення нових клітинок'''
@@ -190,6 +215,7 @@ def new_box(board):
     if count == 1:  # Перевірка, чи з'явився лише один новий квадратик
         return board
 
+
 def game_over_check(board):
     """функція перевірки програшу"""
     # Перевірка, чи є вільні клітини
@@ -199,20 +225,21 @@ def game_over_check(board):
     for i in range(4):
         for j in range(4):
             if (i < 3 and board[i][j] == board[i + 1][j]) or \
-               (j < 3 and board[i][j] == board[i][j + 1]):
+                    (j < 3 and board[i][j] == board[i][j + 1]):
                 return False
     return True
+
 
 def take_turn(direc, board):
     '''функція руху клітинок, перевірка перемоги а також переписування найвищого рахунку якщо він менший за поточний '''
     global score
     global game_over
-    global height_score 
+    global height_score
     global screen
 
     merged = [[False for _ in range(4)] for _ in range(4)]
 
-    if direc == "UP":#рух клітинок в верх
+    if direc == "UP":  # рух клітинок в верх
         for j in range(4):
             for i in range(1, 4):
                 if board[i][j] != 0:
@@ -227,8 +254,8 @@ def take_turn(direc, board):
                         score += board[row - 1][j]
                         board[row][j] = 0
                         merged[row - 1][j] = True
- 
-    elif direc == "DOWN":#рух клітинок вниз
+
+    elif direc == "DOWN":  # рух клітинок вниз
         for j in range(4):
             for i in range(2, -1, -1):
                 if board[i][j] != 0:
@@ -240,11 +267,11 @@ def take_turn(direc, board):
                         board[i][j] = 0
                     if row < 3 and board[row + 1][j] == board[row][j] and not merged[row + 1][j]:
                         board[row + 1][j] *= 2
-                        score += board[row + 1][j]                     
+                        score += board[row + 1][j]
                         board[row][j] = 0
                         merged[row + 1][j] = True
 
-    elif direc == "LEFT":#рух клітинок в ліво
+    elif direc == "LEFT":  # рух клітинок в ліво
         for i in range(4):
             for j in range(1, 4):
                 if board[i][j] != 0:
@@ -258,9 +285,9 @@ def take_turn(direc, board):
                         board[i][col - 1] *= 2
                         score += board[i][col - 1]
                         board[i][col] = 0
-                        merged[i][col - 1] = True 
+                        merged[i][col - 1] = True
 
-    elif direc == "RIGHT":#рух клітинок в право
+    elif direc == "RIGHT":  # рух клітинок в право
         for i in range(4):
             for j in range(2, -1, -1):
                 if board[i][j] != 0:
@@ -272,11 +299,11 @@ def take_turn(direc, board):
                         board[i][j] = 0
                     if col < 3 and board[i][col + 1] == board[i][col] and not merged[i][col + 1]:
                         board[i][col + 1] *= 2
-                        score += board[i][col + 1] 
+                        score += board[i][col + 1]
                         board[i][col] = 0
                         merged[i][col + 1] = True
 
-    if score > height_score:#перевірка якщо рахунок більший за найвищий рахунок то найвищий рахунок набуває значення поточного рахунку
+    if score > height_score:  # перевірка якщо рахунок більший за найвищий рахунок то найвищий рахунок набуває значення поточного рахунку
         height_score = score
         file = open("height_score", "w")
         file.write(f'{height_score}')
@@ -285,12 +312,13 @@ def take_turn(direc, board):
     if game_over_check(board):
         game_over = True
 
-    if 2048 in [tile for row in board for tile in row]:#перевірка чи є клітинка з цифрою 2048 і якщо є то зміна екрану на екран win
+    if 2048 in [tile for row in board for tile in
+                row]:  # перевірка чи є клітинка з цифрою 2048 і якщо є то зміна екрану на екран win
         screen = 'win'
     return board
 
 
-screen = 'menu'
+screen = 'loading'
 music_playing = True
 game_start = False
 run = True
@@ -304,7 +332,7 @@ while run:
     if screen == 'menu':
         # window.fill(colors["light_yellow"])
         window.blit(menu_img, (0, 0))
-        for button in buttons:
+        for button in buttons_menu:
             button.draw(window)
 
     elif screen == 'game':
@@ -317,23 +345,22 @@ while run:
             direction = ""
 
     elif screen == 'gameover':
-        window.fill('#E8E8E8')
-        font_game_over = pygame.font.Font('calibri-font-family\calibri-bold.ttf', 40)
-        game_over_text = font_game_over.render("Game Over!", True, 'black')
-        window.blit(game_over_text, (150, 250))
-        restart_button.draw(window)
-        exit_button.draw(window)
-        menu_button.draw(window)
+        # window.fill('#E8E8E8')
+        window.blit(lose_img, (0, 0))
+        for button in buttons_gameover:
+            button.draw(window)
 
     elif screen == 'win':
-        window.fill('#E8E8E8')
-        font_win = pygame.font.Font('calibri-font-family\calibri-bold.ttf', 40)
+        # window.fill('#E8E8E8')
+        window.blit(win_img, (0, 0))
+        font_win = pygame.font.Font('calibri-font-family\calibri-bold.ttf', 50)
         winning_text = font_win.render("You Win!", True, 'black')
         window.blit(winning_text, (150, 250))
-        menu_button.draw(window)
+        for button in buttons_win:
+            button.draw(window)
 
     elif screen == 'settings':
-        window.blit(setting_img, (0,0))
+        window.blit(setting_img, (0, 0))
         font_text = pygame.font.Font('calibri-font-family\calibri-bold.ttf', 25)
         text1 = font_text.render("Use \u2192, \u2190, \u2191, \u2193 to move. ", True, 'black')
         text2 = font_text.render("Use button 'music' to turn on or off music. ", True, 'black')
@@ -341,7 +368,14 @@ while run:
         window.blit(text1, (75, 50))
         window.blit(text2, (25, 100))
         window.blit(text3, (125, 150))
-        menu_button.draw(window)
+        for button in buttons_settings:
+            button.draw(window)
+
+    elif screen == 'loading':
+        window.blit(loading_img, (0, 0))
+        font_loading = pygame.font.Font('calibri-font-family\calibri-bold.ttf', 50)
+        loading_text = font_loading.render("Click anywhere to start. ", True, 'black')
+        window.blit(loading_text, (10, 525))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -351,25 +385,26 @@ while run:
                 screen = 'menu'
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if screen == 'menu':
-                for button in buttons:
+                for button in buttons_menu:
                     if button.is_clicked(mouse_pos):
                         button.action()
             elif screen == 'gameover':
-                if restart_button.is_clicked(mouse_pos):
-                    restart_button.action()
-                elif exit_button.is_clicked(mouse_pos):
-                    exit_button.action()
-                elif menu_button.is_clicked(mouse_pos):
-                    menu_button.action()
+                for button in buttons_gameover:
+                    if button.is_clicked(mouse_pos):
+                        button.action()
             elif screen == 'game':
                 if music_btn.is_clicked(mouse_pos):
                     music_btn.action()
             elif screen == 'win':
-                if menu_button.is_clicked(mouse_pos):
-                    menu_button.action()
+                for button in buttons_win:
+                    if button.is_clicked(mouse_pos):
+                        button.action()
             elif screen == 'settings':
-                if menu_button.is_clicked(mouse_pos):
-                    menu_button.action()
+                for button in buttons_settings:
+                    if button.is_clicked(mouse_pos):
+                        button.action()
+            elif screen == 'loading':
+                screen = 'menu'
 
         elif event.type == pygame.KEYDOWN:
             if screen == 'game':
@@ -377,12 +412,13 @@ while run:
                     direction = "UP"
                 elif event.key == pygame.K_DOWN:
                     direction = "DOWN"
-                elif event.key == pygame.K_RIGHT:
-                    direction = "RIGHT"
                 elif event.key == pygame.K_LEFT:
                     direction = "LEFT"
+                elif event.key == pygame.K_RIGHT:
+                    direction = "RIGHT"
+            elif event.key == pygame.K_x:
+                screen = 'menu'
 
-    if game_over:
-        screen = 'gameover'
+    pygame.display.update()
 
-    pygame.display.flip()
+pygame.quit()
